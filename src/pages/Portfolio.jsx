5221a5965a44db2cd18bb4235b368cbac3182226
MarcelinaFernando <marcelinafernanda235@gmail.com>
2025-08-31 08:54:06 +0100

    feat: reuse SectionHeader in Portfolio page )

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,11 @@
+import PortfolioHeaderSection from "../components/PortfolioHeaderSection";
+
 export default function Portfolio() {
   return (
     <main>
-      <h1 style={{ color: "red" }}>Meu Portfólio</h1>
-      <p>Aqui você vai encontrar os meus projetos e trabalhos.</p>
-      <p>Adicionei mais texto para ver como fica com o Header e o Footer.</p>
+      <PortfolioHeaderSection />
+      {/* aqui depois entram as imagens do portfolio */}
     </main>
   );
 }
+
