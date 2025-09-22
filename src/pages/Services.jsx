76a3833dd984d3e965c7a2b184124b1ff07c24f1
MarcelinaFernando <marcelinafernanda235@gmail.com>
2025-09-22 17:54:47 +0100

    feat: add PlanCard section on services page (mobile & desktop)

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,8 +1,55 @@
 // src/pages/Services.jsx
 import "./Services.css";
 import SectionHeader from "../components/SectionHeader";
-
+import PlanCard from "../components/PlanCard";
 export default function Services() {
+
+// Plan Details
+const plansData = [
+  {
+    id: 1,
+    title: "Basic",
+    price: "$100",
+    period: "month",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nemo hic quos, ab, dolor aperiam",
+    features: [
+      "Lorem ipsum dolor sit",
+      "Lorem ipsum dolor sit",
+      "Lorem ipsum dolor sit",
+      "Lorem ipsum dolor sit",
+      "Lorem ipsum dolor sit",
+    ],
+  },
+  {
+    id: 2,
+    title: "Plus",
+    price: "$250",
+    period: "month",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nemo hic quos, ab, dolor aperiam",
+    features: [
+      "Lorem ipsum dolor sit",
+      "Lorem ipsum dolor sit",
+      "Lorem ipsum dolor sit",
+      "Lorem ipsum dolor sit",
+      "Lorem ipsum dolor sit",
+    ],
+  },
+  {
+    id: 3,
+    title: "Pro",
+    price: "$400",
+    period: "month",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nemo hic quos, ab, dolor aperiam",
+    features: [
+      "Lorem ipsum dolor sit",
+      "Lorem ipsum dolor sit",
+      "Lorem ipsum dolor sit",
+      "Lorem ipsum dolor sit",
+      "Lorem ipsum dolor sit",
+    ],
+  },
+];
+
   return (
     <main className="services-page">
       {/* Our Services section*/}
@@ -17,6 +64,16 @@ export default function Services() {
           </>
         }
       />
+
+      
+      {/*Plans Section*/}
+      <section className="plans-section">
+        <div className="plans-container">
+        {plansData.map((plan) => (
+          <PlanCard key={plan.id} {...plan} />
+        ))}
+        </div>
+      </section>
     </main>
   );
 }
